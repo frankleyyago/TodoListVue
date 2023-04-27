d5fe55f507c44230a736df83e728ca84afd39063
Yago Frankley <frankleyyago@outlook.com>
2023-04-27 17:28:29 -0300

    creating components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { reactive } from 'vue';
+  import { reactive } from 'vue';
+  import Header from './components/Header.vue'
+  import Form from './components/Form.vue'
+  import TasksList from './components/TasksList.vue'
 
 const state = reactive({
   filter: 'all',
@@ -53,44 +56,8 @@ const getFilteredTasks = () => {
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>
-        Você possui {{ getPendentTasks().length }} tarefas pendentes
-      </p>
-    </header>
-    <form @submit.prevent="addTask">
-      <div class="row">
-        <div class="col">
-          <input :value='state.tempTask' @change="event => state.tempTask = event.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="event => state.filter = event.target.value" class="form-control">
-            <option value="all">Todas as tarefas</option>
-            <option value="pendent">Pendentes</option>
-            <option value="finished">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="task in getFilteredTasks()">
-        <input @change="event => task.finished = event.target.checked" :checked="task.finished" :id="task.title" type="checkbox">
-        <label :class="{ done: task.finished }" class="ms-3" :for="task.title">
-          {{ task.title }}
-        </label>
-      </li>
-    </ul>
+    <Header :pendent-tasks="getPendentTasks().length" />
+    <Form :filter-switcher="event => state.filter = event.target.value" :temp-task="state.tempTask" filter-switcher="event => state.tempTask = event.target.value" :add-task="addTask" />
+    <TasksList :tasks="getFilteredTasks()" />
   </div>
 </template>
-
-<style scoped>
-
-  .done {
-    text-decoration: line-through;
-  }
-
-</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['addTask', 'tempTask', 'filterSwitcher', 'tempTaskEditor'])
+</script>
+<template>
+    <form @submit.prevent="props.addTask">
+        <div class="row">
+            <div class="col">
+                <input :value='props.tempTask' @change="props.tempTaskEditor" required type="text"
+                    placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.filterSwitcher" class="form-control">
+                    <option value="all">Todas as tarefas</option>
+                    <option value="pendent">Pendentes</option>
+                    <option value="finished">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
